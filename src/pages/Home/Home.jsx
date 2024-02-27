7752f1d6e4e93bd0dbcec5abebf831cc5b18bc0a
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2024-02-27 18:11:31 +0600

    change the banner design

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,28 @@
 const Home = () => {
   return (
-    <div>
-      <div
-        className="hero min-h-screen"
-        style={{
-          backgroundImage: "url(https://i.ibb.co/hY4WMhz/payment.jpg)",
-        }}
-      >
-        <div className="hero-overlay bg-opacity-60"></div>
-        <div className="hero-content text-center text-neutral-content">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">Tpayment</h1>
+    <div className="container mx-auto">
+      <div className="flex gap-10 justify-center items-center">
+        <div className="w-1/2">
+          <div className="font-semibold text-4xl space-y-2">
+            <p>
+              Unlock Seamless Transactions with{" "}
+              <span className="text-yellow-500"> Tranzy</span>{" "}
+            </p>
+            <p>Your Wallet&apos;s New Best Friend!</p>
+            <p className="text-base ">
+              Say goodbye to the hassle of carrying cash or fumbling through
+              cards. <span className="text-yellow-500"> Tranzy</span> brings you the ultimate convenience, allowing you
+              to make payments swiftly and securely with just a tap. Whether
+              you&apos;re shopping online or in-store, sending money to friends, or
+              splitting bills, SwiftPay streamlines your transactions, making
+              every financial interaction effortless. Experience seamless
+              transactions like never before - download SwiftPay today and
+              redefine the way you pay!
+            </p>
           </div>
+        </div>
+        <div className="w-[40rem]">
+          <img src="https://i.ibb.co/tbrM5VK/4060173.jpg" alt="" />
         </div>
       </div>
     </div>
--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -22,61 +22,67 @@ const Navbar = () => {
       });
   };
   return (
-    <div>
-      <div className="navbar bg-black text-white">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+    <div className=" bg-slate-300">
+      <div className="container mx-auto">
+        <div className="navbar">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost lg:hidden"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                <li className="text-base font-semibold">
+                  <Link to="/">Home</Link>
+                </li>
+
+                <li>
+                  <Link to="/dashboard">Dashboard</Link>
+                </li>
+              </ul>
             </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-            >
-              <li>
+            <a className=" text-4xl font-bold text-yellow-500"> Tranzy</a>
+          </div>
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">
+              <li className="text-base font-semibold">
                 <Link to="/">Home</Link>
               </li>
 
-              <li>
+              <li className="text-base font-semibold">
                 <Link to="/dashboard">Dashboard</Link>
               </li>
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">Tpayment</a>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-
-            <li>
-              <Link to="/dashboard">Dashboard</Link>
-            </li>
-          </ul>
-        </div>
-        <div className="navbar-end">
-          {user && !loading ? (
-            <p className="text-white">{user?.displayName}</p>
-          ) : (
-            <Link to="/login" className="btn">
-              Login
-            </Link>
-          )}
-          <button onClick={handleLogout}>LogOut</button>
+          <div className="navbar-end">
+            {user && !loading ? (
+              <p className="text-white">{user?.displayName}</p>
+            ) : (
+              <Link to="/login" className="btn">
+                Login
+              </Link>
+            )}
+            <button onClick={handleLogout} className="btn">LogOut</button>
+          </div>
         </div>
       </div>
     </div>
